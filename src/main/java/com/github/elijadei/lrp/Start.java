a3f9198daaf1472694a14f29522abc110b7ae5ec
dei <panik772@gmail.com>
2019-09-03 07:59:39 +0200

    new line

--- src/main/java/com/github/elijadei/lrp/Start.java
+++ src/main/java/com/github/elijadei/lrp/Start.java
@@ -3,5 +3,6 @@ package com.github.elijadei.lrp;
 public class Start {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("jjgfmfjfj");
     }
 }
