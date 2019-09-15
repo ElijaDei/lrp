943746e97e9ac2febc7bad63dd46b4da2592fa46
dei <panik772@gmail.com>
2019-09-15 18:37:12 +0200

    Update with RandomSampling

---
+++ src/main/java/com/github/elijadei/lrp/clusterizing/Clusterizer.java
@@ -0,0 +1,40 @@
+package com.github.elijadei.lrp.clusterizing;
+
+import com.github.elijadei.lrp.Node;
+import com.github.elijadei.lrp.Nodes;
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Clusterizer {
+
+    private List<Node> nodes;
+
+    private int clusterSize;
+
+    public Clusterizer(List<Node> nodes, int clusterSize) {
+        this.nodes = nodes;
+        this.clusterSize = clusterSize;
+    }
+
+
+    public List<Nodes> getClusteredNodes() {
+        KMeansPlusPlusClusterer<Node> clusterer = new KMeansPlusPlusClusterer<>(calculateCountOfClusterPoints());
+        List<CentroidCluster<Node>> cluster = clusterer.cluster(nodes);
+        return mapToNodes(cluster);
+    }
+
+    private int calculateCountOfClusterPoints() {
+        return (int) Math.sqrt(clusterSize);
+    }
+
+
+    private List<Nodes> mapToNodes(List<CentroidCluster<Node>> cluster) {
+        return cluster.stream()
+                .map(Nodes::fromCluster)
+                .collect(Collectors.toList());
+    }
+
+}
